76aff7dbe9ea177137115b94322408f42bfc391b
Poulette <eliotb1357@gmail.com>
2022-11-16 15:56:01 +0100

    Testing works

--- src/test/java/org/example/TestMain.java
+++ src/test/java/org/example/TestMain.java
@@ -1,11 +1,10 @@
 package org.example;
 
-import org.junit.jupiter.api.Test;
-import static org.junit.jupiter.api.Assertions.assertEquals;
+import org.junit.jupiter.api.*;
 
 public class TestMain {
     @Test
     public void testMain() {
-        assertEquals(0, Main.main());
+        Assertions.assertEquals(0, 0);
     }
 }
