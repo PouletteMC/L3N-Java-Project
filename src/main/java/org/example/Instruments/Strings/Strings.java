0ed454815e18a497e7fb078208cf7431ba7cb0c7
Poulette <eliotb1357@gmail.com>
2022-11-30 21:16:11 +0100

    removed bad finals

--- src/main/java/org/example/Instruments/Strings/Strings.java
+++ src/main/java/org/example/Instruments/Strings/Strings.java
@@ -5,8 +5,8 @@ import org.example.Instruments.Instruments;
 // A string instrument is a type of instrument
 public interface Strings extends GlobalMethods {
     // These values are not expected to change during the lifetime of the object, so they are final
-    final int length = 0;
-    final int width = 0;
+    int length = 0;
+    int width = 0;
 
     int getLength();
     int getWidth();
--- src/main/java/org/example/Instruments/Woodwinds/Woodwinds.java
+++ src/main/java/org/example/Instruments/Woodwinds/Woodwinds.java
@@ -6,6 +6,6 @@ import org.example.Instruments.Instruments;
 // A woodwind instrument is a type of instrument
 public interface Woodwinds extends GlobalMethods {
     // The type is not expected to change, so it is final
-    final String type = null;
+    String type = null;
     String getType();
 }
