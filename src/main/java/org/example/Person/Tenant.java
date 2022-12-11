54d3c6fd08b76a901813de4d54e73401155e1b3f
Poulette <eliotb1357@gmail.com>
2022-12-11 18:17:42 +0100

    Added JavaDoc

--- src/main/java/org/example/Person/Tenant.java
+++ src/main/java/org/example/Person/Tenant.java
@@ -1,8 +1,65 @@
 package org.example.Person;
 
+import org.example.Buildings.House;
+
+/**
+ * @authors: Eliot Barbe, Sebastian Pestana, Adam N'Mily, Laura Ghostine
+ * @version: 1.0
+ */
+
 public class Tenant extends Person {
 
+    private House house;
+
+    /**
+     * Constructor of the Tenant class without a house
+     * @param name
+     * @param surname
+     * @param age
+     * @see Person#Person(String, String, int)  Person
+     */
     public Tenant(String name, String surname, int age) {
         super(name, surname, age);
+    }
+
+    /**
+     * Constructor with a house
+     * @param name
+     * @param surname
+     * @param age
+     * @param house
+     * @see Person#Person(String, String, int)  Person
+     */
+    public Tenant(String name, String surname, int age, House house) {
+        super(name, surname, age);
+        this.house = house;
+    }
+
+    /**
+     * @return the house
+     * @see House
+     * @since 1.0
+     */
+    public House getHouse() {
+        return house;
+    }
+
+    /**
+     * @param house the new house
+     * @see House
+     * @since 1.0
+     */
+    public void setHouse(House house) {
+        this.house = house;
+    }
+
+    /**
+     * @return the name of the tenant
+     * @see Person#getName()
+     * @see Person#getSurname()
+     * @see Person#getAge()
+     */
+    public String toString() {
+        return "Tenant: " + getName() + " " + getSurname() + ", " + getAge();
     }
 }
--- src/test/java/org/example/Buildings/TestBar.java
+++ src/test/java/org/example/Buildings/TestBar.java
@@ -2,12 +2,17 @@ package org.example.Buildings;
 
 import org.junit.jupiter.api.*;
 
+/**
+ * @author: Sebastian Pestana
+ * @version: 1.0
+ */
+
 @DisplayName("Test Bar")
 public class TestBar {
     @Test
     @DisplayName("The bar has a capacity")
     public void TestBarCapacity(){
-        Bar bar = 
+        Bar bar;
     }
 
 }
