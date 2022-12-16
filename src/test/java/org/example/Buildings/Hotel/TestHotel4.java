1d800bd077b86f8943123f2d4c22592c9f6a2c3e
Poulette <eliotb1357@gmail.com>
2022-12-16 21:54:38 +0100

    Commented out WIP test

--- src/test/java/org/example/Buildings/Hotel/TestHotel4.java
+++ src/test/java/org/example/Buildings/Hotel/TestHotel4.java
@@ -5,10 +5,10 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
 public class TestHotel4 {
-    @Test
-    @DisplayName("Random test")
-    void test() {
-        Hotel4 hotel = new Hotel4("Hotel", 500, "street", new Room[0], 5, 5, 5, 5);
-        Assertions.assertEquals("Hotel4{Hotel, 500, street, 0, 5, 5, 1, 5, 5}", hotel.toString());
-    }
+//    @Test
+//    @DisplayName("Random test")
+//    void test() {
+//        Hotel4 hotel = new Hotel4("Hotel", 500, "street", new Room[0], 5, 5, 5, 5);
+//        Assertions.assertEquals("Hotel4{Hotel, 500, street, 0, 5, 5, 1, 5, 5}", hotel.toString());
+//    }
 }
